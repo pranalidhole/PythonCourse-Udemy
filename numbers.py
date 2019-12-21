2e3eb963e78675da9f7bd36ba8e1f02b7df57980
pranalidhole <pranali.dhole74@gmail.com>
2019-12-21 20:50:25 +0530

    numbers.py added

---
+++ numbers.py
@@ -0,0 +1,20 @@
+number1 = 20
+number2 = 50
+sum = number1 + number2
+print(sum)
+diff = number2 - number1
+print(diff)
+mult = number1*number2
+print(mult)
+div = number2/number1
+print(div)
+pow = number2 ** number1   #power of
+print(pow)
+number3 = -80
+number4 = 100
+sum1 = number3 + number4
+print(sum1)
+a = 2 * number1 + number2
+print(a)
+b = 2 * (number1 + number2)
+print(b)
