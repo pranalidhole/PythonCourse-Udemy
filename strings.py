fa45da61de0df750a2ec59f8e96b6885d761e527
pranalidhole <pranali.dhole74@gmail.com>
2019-12-22 14:28:12 +0530

    strings.py added

---
+++ strings.py
@@ -0,0 +1,19 @@
+name = "my name is pranali"
+print(name)
+date = "27 January 1993"
+print(date)
+longString = "this is a long sring snd can be of any length"
+print(longString)
+shortString = "a"
+print(shortString)
+print(len(longString))
+#indexing starts from zero in strings
+#reverse indexing starts from -1 in strings
+#operators in string:
+print(name + date)
+print(name * 5)
+print(date[3])
+print(date[3:10]) #upper limit is not included... it will print till 9 i.e. 3:9.. so always add upper limit
+print(name[:2]) # will strt from the 0 index and end at upper limit - 1 i.e. 1
+print(name[5:])
+print(date[-12:-5]) #reverse indexing
